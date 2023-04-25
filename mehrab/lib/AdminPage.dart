d4337b5d85ec99281c1669ad8d9f2f9f92e1f6e1
Lamaalshaya <lamaalshaya5@gmail.com>
2023-04-25 19:23:14 +0300

    Add mosque..etc worked

--- mehrab/lib/AdminPage.dart
+++ mehrab/lib/AdminPage.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/src/material/dropdown.dart';
-
-List<String> itemsList = ['خالد', 'محمد', 'عاصم','صالح','عبدالله'];
-String? selectedItem = 'خالد';
-
+import 'package:mehrab/AddMosque.dart';
+import 'package:mehrab/AdminLogin.dart';
 
 class AdminPage extends StatelessWidget {
-  const AdminPage({Key? key}) : super(key: key);
+  const AdminPage({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Mehrab',
-        home: Scaffold(
+    return Container(
+        
+       
+        child: Scaffold(
             extendBodyBehindAppBar: true,
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
-              leading: const BackButton(
-                color: Colors.white, // <-- SEE HERE
-              ),
-              title: Align(
-                alignment: Alignment.centerRight,
-                child: Text(
-                  ' إضافة مسجد جديد',
-                  style: TextStyle(fontFamily: 'Elmessiri'),
-                ),
-              ),
+              title: Text('الرئيسية '),
               backgroundColor: Color.fromARGB(255, 20, 5, 87),
-            ),
+              
+             actions: <Widget>[
+                           IconButton(
+                              icon: Icon(
+                                    Icons.logout_rounded,
+                                    color: Colors.white,
+        
+                                       ),
+                                   onPressed: () { Navigator.pop(context);}, 
+                              )
+                            ],
+                         ),
+
             body: Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
@@ -44,216 +44,58 @@ class AdminPage extends StatelessWidget {
                   children: <Widget>[
                     Container(
                       padding: const EdgeInsets.fromLTRB(20, 40, 20, 40),
-                      
-                    ),
-                    
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90.0),
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(255, 20, 5, 87),
-                                  width: 1),
-                            ),
-                            labelText: ' معرِّف المسجد',
-                            labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 20, 5, 87),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: TextField(
-                          //obscureText: true,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90.0),
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(255, 20, 5, 87),
-                                  width: 1),
-                            ),
-                            labelText: ' اسم المسجد',
-                            labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 20, 5, 87),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-
-
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: TextField(
-                          //obscureText: true,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90.0),
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(255, 20, 5, 87),
-                                  width: 1),
-                            ),
-                            labelText: '  الحي',
-                            labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 20, 5, 87),
-                            ),
-                          ),
-                        ),
+                      child: Image.asset(
+                        'images/logo4.png',
+                        height: 200,
+                        width: 230,
                       ),
                     ),
 
                     const SizedBox(height: 20,),
                     
-
-
-                    /* Container(
-                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90.0),
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(255, 20, 5, 87),
-                                  width: 1),
-                            ), 
-                            labelText: '  اختر إمام المسجد',
-                            labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 20, 5, 87),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ), */ 
-
                     
-                      Container(
-                        decoration:  BoxDecoration(
-                                   borderRadius: BorderRadius.circular(90.0),
-                                   border: Border.all(color:Color.fromARGB(255, 20, 5, 87), width: 1),
-                                ), 
-                        width: 370,
-                        height: 60, 
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: DropdownButtonFormField<String>(
-                          decoration: InputDecoration( 
-                            labelText: 'اختر إمام المسجد',
-                            labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 20, 5, 87),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.accessibility_rounded,
-                              color: Color.fromARGB(255, 20, 5, 87), 
-                            ),
-                             /* border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90.0),
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(255, 20, 5, 87),
-                                  width: 1),
-                            ), */
-
-                          ),
-                          iconSize: 25,
-                          icon: Icon(Icons.arrow_drop_down_circle, color: Color.fromARGB(255, 20, 5, 87)),
-                              borderRadius: BorderRadius.circular(20), // هذا الراديس حق الخيارات
-                                value: selectedItem,
-                                items: itemsList
-                                .map((item) => DropdownMenuItem(
-                                  value: item,
-                                  child: Text(item, style: TextStyle(fontSize: 15))))
-                                  .toList(),
-                                  onChanged: (item) => setState(() => selectedItem = item)
-                                  ),
-                                  ),
-                                  ),
-
-                       const SizedBox(height: 20,), // this line insert a space between elements
-
-                     Container(
-                        decoration:  BoxDecoration(
-                                   borderRadius: BorderRadius.circular(90.0),
-                                   border: Border.all(color:Color.fromARGB(255, 20, 5, 87), width: 1),
-                                ), 
-                        width: 370,
-                        height: 60, 
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: DropdownButtonFormField<String>(
-                          decoration: InputDecoration( 
-                            labelText: 'اختر مؤذن المسجد',
-                            labelStyle: TextStyle(
-                              color: Color.fromARGB(255, 20, 5, 87),
-                            ),
-                            prefixIcon: Icon(
-                              Icons.accessibility_rounded,
-                              color: Color.fromARGB(255, 20, 5, 87), 
-                            ),
-                             /* border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(90.0),
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(255, 20, 5, 87),
-                                  width: 1),
-                            ), */
-
-                          ),
-                          iconSize: 25,
-                          icon: Icon(Icons.arrow_drop_down_circle, color: Color.fromARGB(255, 20, 5, 87)),
-                              borderRadius: BorderRadius.circular(20), // هذا الراديس حق الخيارات
-                                value: selectedItem,
-                                items: itemsList
-                                .map((item) => DropdownMenuItem(
-                                  value: item,
-                                  child: Text(item, style: TextStyle(fontSize: 15))))
-                                  .toList(),
-                                  onChanged: (item) => setState(() => selectedItem = item)),
-                      ),
-                      ),
-                      
-
-                      const SizedBox(height: 20,),
 
 
                     Container(
                         height: 80,
-                        padding: const EdgeInsets.all(20),
+                        width: 300,
+                        padding: const EdgeInsets.all(10),
                         child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                           // minimumSize: const Size.fromHeight(50),
+                            //minimumSize: const Size.fromHeight(50),
                             primary: Color.fromARGB(255, 20, 5, 87),
                           ),
-                          child: const Text(' إضافة المسجد',
-                              style: TextStyle(fontFamily: 'Elmessiri')),
+                          child: const Text(' إضافة مسجد',
+                              style: TextStyle(fontFamily: 'Elmessiri', fontSize: 20)),
+                          onPressed: () {
+                    Navigator.push(
+                      context, MaterialPageRoute(
+                          builder: (context) => const AddMosque()),
+                    );
+                  },
+                        )),
+
+                        const SizedBox(height: 20,),
+
+                        Container(
+                        height: 80,
+                        width: 300,
+                        padding: const EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            //minimumSize: const Size.fromHeight(50),
+                            primary: Color.fromARGB(255, 20, 5, 87),
+                          ),
+                          child: const Text(' إنشاء حسابات المؤذن و الإمام',
+                              style: TextStyle(fontFamily: 'Elmessiri', fontSize: 20)),
                           onPressed: () {},
                         )),
+
+                        
                     
                   ],
                 ),
               ),
             )));
   }
-  
-  setState(String? Function() param0) {}
-
-
-
 }
-
-
-
-
