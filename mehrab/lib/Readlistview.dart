82967e19d02d8c096803dce810f91ce9d967cf91
Jayesso <jummana.128@gmail.com>
2023-05-16 18:14:37 +0300

    last updated list and details UI

--- mehrab/lib/Readlistview.dart
+++ mehrab/lib/Readlistview.dart
@@ -27,7 +27,7 @@ navigateToDetail(DocumentSnapshot post){
 Widget build (BuildContext context){
   return Scaffold(
 appBar: AppBar(
-  shape: RoundedRectangleBorder(
+shape: RoundedRectangleBorder(
 
 borderRadius: BorderRadius.vertical(
 bottom: Radius.circular(20),
@@ -35,9 +35,8 @@ bottom: Radius.circular(20),
  ),
 
 ),
-
+centerTitle: true,
 title: Align(
-            alignment:Alignment.centerRight,
             child:Text(
            "قائمة المساجد",
            style: TextStyle(fontFamily: 'Elmessiri'),
@@ -47,9 +46,10 @@ title: Align(
   automaticallyImplyLeading: false,
   backgroundColor:  Color.fromARGB(255, 20, 5, 87),
         ),
-       
+ 
     bottomNavigationBar: BottomNavigationBar(
-    selectedItemColor: Colors.grey,
+    currentIndex: 3,
+    selectedItemColor: Colors.blue,
     unselectedItemColor: Colors.grey,
     type: BottomNavigationBarType.fixed,
     items: const <BottomNavigationBarItem>[
@@ -70,68 +70,120 @@ title: Align(
       ),
 
        BottomNavigationBarItem(
-        icon: Icon(Icons.home,color: Colors.blue),
+        icon: Icon(Icons.home),
         label: 'الرئيسية',
+
+     
       ),
       
 
     ],
   ),
 
-body:StreamBuilder(  
-        stream:_mosque,
-        builder:(context, snapshot){
-          if(snapshot.hasError){
-           return const Text('مشكلة بالاتصال');
-          }
-         if(snapshot.connectionState== ConnectionState.waiting){
-          return const Text('...جاري التحميل');
-          }
-         if (snapshot.data == null) {
-        return const Text('no data');
-         }
-
-        var docs = snapshot.data!.docs;
-
-
-         return ListView.builder(
-           padding: EdgeInsets.only(top: 20.0),
-            itemCount: docs.length,
-            
-            itemBuilder: (context, index){
-//boxShadow: [
-//      BoxShadow(color: Colors.black.withOpacity(50), blurRadius: 50),
-//    ];
-            return Card(child : ListTile(
-              
-              shape: RoundedRectangleBorder( 
-              side: BorderSide(width: 1, color:Color.fromARGB(255, 213, 213, 213)),
-              borderRadius: BorderRadius.circular(10),
+body:Container(
+decoration: BoxDecoration(
+image: DecorationImage(
+image: AssetImage("images/background2.jpg"),
+                  fit: BoxFit.cover,
                 ),
-          
-            
-      onTap: () {
-      navigateToDetail(docs[index]);
-      },
-          trailing: Container(
-            width:80,
-            height: 80,
-            child: ClipRRect(
-            borderRadius: BorderRadius.circular(18),
-            child: Image.network(docs[index]['Image'], 
-            fit: BoxFit.cover ,
-                  ),
-                 ),
-          ), 
-              title: Text(docs[index]['Name'],textAlign: TextAlign.right, style: TextStyle(fontFamily: 'Elmessiri'),),
-              subtitle:Text ( 'حي '+docs[index]['District'] ,textAlign: TextAlign.right,  style: TextStyle(fontFamily: 'Elmessiri'),),
+              ),
+  child:StreamBuilder(  
+  
+          stream:_mosque,
+  
+          builder:(context, snapshot){
+  
+            if(snapshot.hasError){
+  
+             return const Text('مشكلة بالاتصال');
+  
+            }
+  
+           if(snapshot.connectionState== ConnectionState.waiting){
+  
+            return const Text('...جاري التحميل');
+  
+            }
+  
+           if (snapshot.data == null) {
+  
+          return const Text('no data');
+  
+           }
+  
+  
+  
+          var docs = snapshot.data!.docs;
+  
+  
+  
+  
+  
+           return ListView.builder(
+  
+             padding: EdgeInsets.only(top: 20.0),
+  
+              itemCount: docs.length,
+  
               
-              ),);
-               
-            });
-        }
+  
+              itemBuilder: (context, index){
+  
+  
+  
+              return Card(child : ListTile(
+  
+                
+  
+                shape: RoundedRectangleBorder( 
+  
+                side: BorderSide(width: 1, color:Color.fromARGB(255, 213, 213, 213)),
+  
+                borderRadius: BorderRadius.circular(10),
+  
+                  ),         
+  
+              
+  
+        onTap: () {
+  
+        navigateToDetail(docs[index]);
+  
+        },
+  
+            trailing: Container(
+  
+              width:80,
+  
+              height: 80,
+  
+              child: ClipRRect(
+  
+              borderRadius: BorderRadius.circular(18),
+  
+              child: Image.network(docs[index]['Image'], 
+  
+              fit: BoxFit.cover ,
+  
+                    ),
+  
+                   ),
+  
+            ), 
+  
+                title: Text(docs[index]['Name'],textAlign: TextAlign.right, style: TextStyle(fontFamily: 'Elmessiri'),),
+                subtitle:Text ( 'حي '+ docs[index]['District'] ,textAlign: TextAlign.right,  style: TextStyle(fontFamily: 'Elmessiri'),),
+  
+                
+  
+                ),);
+              });
+          }
+
+  
+          ),
+),
         
-        ),
   );
 }
 }
